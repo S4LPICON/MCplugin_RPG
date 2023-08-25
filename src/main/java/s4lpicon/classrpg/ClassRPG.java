0e85db1a4249078f2134238292a3056c5d8bdeed
S4LPICON <s4lpicong4mer3993@gmail.com>
2023-08-25 16:41:25 -0500

    Pruebas de sincronizacion

---
+++ src/main/java/s4lpicon/classrpg/ClassRPG.java
@@ -0,0 +1,17 @@
+package s4lpicon.classrpg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ClassRPG extends JavaPlugin {
+//asasas
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
