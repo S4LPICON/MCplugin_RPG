a971a438d3507590962c2371c94e3f3f710c1cc8
S4LPICON <s4lpicong4mer3993@gmail.com>
2023-08-25 16:51:23 -0500

    Empezando la configuracion

--- src/main/java/s4lpicon/classrpg/ClassRPG.java
+++ src/main/java/s4lpicon/classrpg/ClassRPG.java
@@ -1,7 +1,8 @@
 package s4lpicon.classrpg;
 
 import org.bukkit.plugin.java.JavaPlugin;
-
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 public final class ClassRPG extends JavaPlugin {
 //asasas
     @Override
@@ -11,8 +12,20 @@ public final class ClassRPG extends JavaPlugin {
 
     }
 
+    private Process getLogger() {
+        return null;
+    }
+
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("miComando")) {
+            sender.sendMessage("¡Hola desde mi comando!");
+            return true;
+        }
+        return false;
     }
 }
